f0b225a6a868088a5de9866bffac57de475992e6
Amélie <ada125@student.bham.ac.uk>
2023-07-22 15:52:00 +0100

    Added scoring to messages @ing the bot

--- bot.js
+++ bot.js
@@ -1,4 +1,50 @@
 require('dotenv').config()
+// https://www.npmjs.com/package/sentiment
+
+
+/*
+ * Sentiment analysis
+ */
+
+
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+
+function getScoreAsString(score) {
+    if (score <= -5) {
+        return "Yikes! This message is extremely negative. 😱😡👎";
+    } else if (score > -5 && score < -3) {
+        return "Oh no! This message is very negative. 😔👎";
+    } else if (score >= -3 && score < -1) {
+        return "Hmm, this message seems negative. 🙁";
+    } else if (score >= -1 && score < 0) {
+        return "A bit negative, but not too bad. 😕";
+    } else if (score === 0) {
+        return "Neutral message. 🤔";
+    } else if (score > 0 && score <= 1) {
+        return "A touch of positivity! 😊";
+    } else if (score > 1 && score <= 3) {
+        return "Hey, this message is quite positive! 😄👍";
+    } else if (score > 3 && score <= 5) {
+        return "Woo-hoo! This message is very positive! 🎉😃👍";
+    } else {
+        return "Wowza! This message is extremely positive! 🌟😍👍";
+    }
+}
+
+
+
+
+function getAnalysis(message){
+    var result = sentiment.analyze(message);
+    return getScoreAsString(result.score)
+}
+
+
+/**
+ * Discord bot implementation
+ */
+
 const { Client, GatewayIntentBits } = require('discord.js')
 const client = new Client({
 	intents: [
@@ -8,6 +54,9 @@ const client = new Client({
 		GatewayIntentBits.GuildMembers,
 	],
 });
+
+const botMentionPattern = /sentiment\s*bot/i;
+
 
 // Function to check for bad words in the message
 function containsBadWord(message) {
@@ -20,11 +69,14 @@ function containsBadWord(message) {
 
 
 
-client.on("ready", () =>{
-    console.log('logged in as ', client.user.tag,'!')
-})
+client.on("ready", () => {
+    console.log('Logged in as', client.user.tag, '!');
+    client.user.setPresence({
+        status: "online",
+    });
+});
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
     if (message.author.bot) return; // Ignore messages sent by bots
 
     console.log(`Message from ${message.author.username}: ${message.content}`);
@@ -38,16 +90,37 @@ client.on("messageCreate", (message) => {
     if (botMentionPattern.test(message.content)) {
         // Check if the message also contains a bad word
         if (containsBadWord(message)) {
-            message.reply("Hi I am Sentiment Bot, my purpose is to tell you if your message has a negative sentiment (i.e. it's a mean message), and this message definitely has a negative sentiment :)");
+            message.reply("Hi, I am Sentiment Bot. My purpose is to tell you if your message has a negative sentiment (i.e., it's a mean message), and this message definitely has a negative sentiment :)");
         } else {
-            message.reply("Hello, I am Sentiment Bot! Nice to meet you! I can tell you if your message has a negative sentiment (i.e. it's a mean message).");
+            message.reply("Hello, I am Sentiment Bot! Nice to meet you! I can tell you if your message has a negative sentiment (i.e., it's a mean message).");
         }
         return; // Exit the function early to avoid further processing
     }
 
     if (containsBadWord(message)) {
-        message.reply("I think this message has a negative sentiment.");
+        message.reply("Uh oh, I think this is a rude message...");
     }
+
+
+
+
+
+
+    
+   // Check if the message is a reply and if the original message contains the bot's ID
+   if (message.reference && message.content.includes(client.user.id)) {
+    try {
+        // Fetch the original message
+        const referencedMessage = await message.channel.messages.fetch(message.reference.messageId);
+        var sentimentResult = getAnalysis(referencedMessage.content);
+        message.channel.send(sentimentResult);
+    } catch (error) {
+        console.error('Error fetching the referenced message:', error);
+        message.reply('There was an error while fetching the referenced message.');
+    }
+} 
+
 });
+
 
 client.login(process.env.TOKEN)
